a62adf53a8bc43707ab31ca50bf2147d31b186d0
Anil <anil6996f@gmail.com>
2024-09-20 14:23:17 +0530

    Implemented getFeaturedProducts controller that first checks redis for the featured products. If not found in redis check in mongodb and then add it in redis for later quick retrieval

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { getAllProducts } from '../controllers/product.controller.js';
+import { getAllProducts,getFeaturedProducts } from '../controllers/product.controller.js';
 import { adminRoute, protectRoute } from '../middleware/auth.middleware.js';
 
 router.get('/', protectRoute, adminRoute, getAllProducts);  // only admin can view all the products.
+router.get('/featured', getFeaturedProducts);
+
 
 export default router;
