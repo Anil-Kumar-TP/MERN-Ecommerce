0feef35f67b72d1ebc13c7905e175c4d914b6616
Anil <anil6996f@gmail.com>
2024-09-20 19:40:07 +0530

    Implemented function to show 3 recommended products

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,10 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { getAllProducts,getFeaturedProducts,createProduct,deleteProduct } from '../controllers/product.controller.js';
+import { getAllProducts,getFeaturedProducts,getRecommendedProducts,getProductsByCategory,createProduct,deleteProduct } from '../controllers/product.controller.js';
 import { adminRoute, protectRoute } from '../middleware/auth.middleware.js';
 
 router.get('/', protectRoute, adminRoute, getAllProducts);  // only admin can view all the products.
 router.get('/featured', getFeaturedProducts);
+router.get('/category/:category', getProductsByCategory);
+router.get('/recommendations', getRecommendedProducts);
 router.post('/', protectRoute, adminRoute, createProduct);
 router.delete('/:id', protectRoute, adminRoute, deleteProduct);
 
